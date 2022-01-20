9118db466a508a4ee4d8095b3bea21c37c45145e
Yubo <mao00071@morris.umn.edu>
2022-01-20 15:34:31 -0600

    yubo

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -21,6 +21,7 @@ public class Hellos {
 
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
+    builder.append(yuboSaysHello());
 
     return builder.toString();
   }
@@ -31,6 +32,10 @@ public class Hellos {
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
+  }
+
+  private String yuboSaysHello() {
+    return "Yubo says 'Hello!'\n";
   }
 
 }

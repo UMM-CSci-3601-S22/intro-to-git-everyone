d9810cd07e5fc1c1aac05cf0dbdf7e4f1269aacc
Joshua Engelkes <enge0479@morris.umn.edu>
2022-01-20 15:25:29 -0600

    Add Josh Hello
    This adds a new hello from Josh that reads: "Josh says 'Hello!'\n"

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -19,12 +19,16 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(joshSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
 
     return builder.toString();
   }
 
+  private String joshSaysHello() {
+    return "Josh says 'Hello!'\n";
+  }
   private String nicSaysHello() {
     return "Nic says 'Howdy!'\n";
   }
